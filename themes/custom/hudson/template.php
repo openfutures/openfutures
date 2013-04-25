b73acc27f71a7060fdcd2279cedd6e566a75b2c3
Peter Cossey <peter.cossey@gmail.com>
2013-04-26 00:32:15 +1000

    UX improvements and QA.

--- themes/custom/hudson/template.php
+++ themes/custom/hudson/template.php
@@ -179,10 +179,7 @@ function hudson_preprocess_page(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("node" in this case.)
  */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_node(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
+function hudson_preprocess_node(&$variables, $hook) {
   // Optionally, run node-type-specific preprocess functions, like
   // STARTERKIT_preprocess_node_page() or STARTERKIT_preprocess_node_story().
   $function = __FUNCTION__ . '_' . $variables['node']->type;
@@ -190,7 +187,28 @@ function STARTERKIT_preprocess_node(&$variables, $hook) {
     $function($variables, $hook);
   }
 }
-// */
+
+/**
+ * Override or insert variables into the scenario node templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("node" in this case.)
+ */
+function hudson_preprocess_node_scenario(&$vars, $hook) {
+  $node = $vars['node'];
+
+  // Add call to action for scenarios missing body text
+  if ($vars['view_mode'] == 'teaser' && empty($vars['body'])) {
+    $markup = array(
+      '#markup' => t("Improve this scenario by !edit.", array('!edit' => l(t('adding a description'), "node/$node->nid/edit"))),
+      '#prefix' => '<p class="body-missing">',
+      '#suffix' => '</p>',
+    );
+    $vars['content']['body'] = $markup;
+  }
+}
 
 /**
  * Override or insert variables into the comment templates.
