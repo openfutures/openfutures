b91be68f5b1316d4bd0112d6bf0091545cfbd645
Peter Cossey <peter.cossey@gmail.com>
2013-10-24 11:32:27 +1100

    [#122] Prepopulate values removed from form action to prevent saving issues.

--- modules/features/library_scenario/library_scenario.js
+++ modules/features/library_scenario/library_scenario.js
@@ -6,30 +6,59 @@
     attach: function(context, settings) {
       // add form js here
       $("a.mode-quick").click(function() {
-        var raw_title = $("#edit-title").val();
-        var title = $.trim(raw_title);
-        var raw_source = $("#edit-field-scenario-source-und-0-value").val();
-        var source = $.trim(raw_source);
+        var title = $("#edit-title").val();
+        var source = $("#edit-field-scenario-source-und-0-value").val();
+        var titleTrimmed = $.trim(title);
+        var editTitle = "";
+        var sourceTrimmed = $.trim(source);
+        var editSource = "";
         // alert("Just took over quick mode. Your title is: "+source);
         if (!window.location.origin)
           window.location.origin = window.location.protocol+"//"+window.location.host;
         var base = window.location.origin;
         var path = "/node/add/scenario/quick";
-        var query_string = '';
-
+        // create query string elements
+        if (titleTrimmed) {
+          editTitle = "?edit[title]="+encodeURIComponent(titleTrimmed);
+          editSource += "&";
+        } else {
+          editSource += "?";
+        }
+        if (sourceTrimmed) {
+          editSource += "edit[field_scenario_source][und][0][value]="+encodeURIComponent(source);
+        } else {
+          editSource = "";
+        }
         // window.location.href = base+path+"?edit[title]="+encodeURIComponent(title)+"&edit[field_scenario_source][und][0][value]="+encodeURIComponent(source);
-        window.location.href = base+path+query_string;
+        window.location.href = base+path+editTitle+editSource;
         return false;
       });
       $("a.mode-expanded").click(function() {
         var title = $("#edit-title").val();
         var source = $("#edit-field-scenario-source-und-0-value").val();
+        var titleTrimmed = $.trim(title);
+        var editTitle = "";
+        var sourceTrimmed = $.trim(source);
+        var editSource = "";
         // alert("Just took over quick mode. Your title is: "+source);
         if (!window.location.origin)
           window.location.origin = window.location.protocol+"//"+window.location.host;
         var base = window.location.origin;
         var path = "/node/add/scenario";
-        window.location.href = base+path+"?edit[title]="+encodeURIComponent(title)+"&edit[field_scenario_source][und][0][value]="+encodeURIComponent(source);
+        // create query string elements
+        if (titleTrimmed) {
+          editTitle = "?edit[title]="+encodeURIComponent(titleTrimmed);
+          editSource += "&";
+        } else {
+          editSource += "?";
+        }
+        if (sourceTrimmed) {
+          editSource += "edit[field_scenario_source][und][0][value]="+encodeURIComponent(source);
+        } else {
+          editSource = "";
+        }
+        // window.location.href = base+path+"?edit[title]="+encodeURIComponent(title)+"&edit[field_scenario_source][und][0][value]="+encodeURIComponent(source);
+        window.location.href = base+path+editTitle+editSource;
         return false;
       });
       // when the checkbox is changed, see if it is checked, if it's checked,
