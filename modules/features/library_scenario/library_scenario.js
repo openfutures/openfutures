3d8456a56318fb5fa974249810383ee25405aab1
Peter Cossey <peter.cossey@gmail.com>
2013-11-06 12:45:36 +1100

    [#127] Global coverage option is now dead.

--- modules/features/library_scenario/library_scenario.js
+++ modules/features/library_scenario/library_scenario.js
@@ -61,21 +61,6 @@
         window.location.href = base+path+editTitle+editSource;
         return false;
       });
-      // when the checkbox is changed, see if it is checked, if it's checked,
-      // clear the value of the location and update the placeholder attribute
-      var updateLocation = function() {
-        var c = $("#edit-field-scenario-global-und").attr("checked");
-        if (c) {
-          $("#edit-field-scenario-location-und").val("");
-          $("#edit-field-scenario-location-und").attr( "placeholder", "Place names disabled due to global checkbox." );
-        } else {
-          // global scenario was just unchecked. If we saved an value update the
-          // element, otherwise put the original placeholder text back.
-          $("#edit-field-scenario-location-und").val("");
-          $("#edit-field-scenario-location-und").attr( "placeholder", "Enter place names separated by a comma..." );
-        }
-      };
-      $("#edit-field-scenario-global-und").change(updateLocation);
     }
   }
 })(jQuery);
